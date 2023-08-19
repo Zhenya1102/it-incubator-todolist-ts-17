d847e8eb152a6f6930e8afa89060f30108c679c8
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-08-20 01:28:24 +0300

    Commit N1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import React, { useCallback, useEffect } from "react";
 import "./App.css";
-import { TodolistsList } from "../features/TodolistsList/TodolistsList";
-import { ErrorSnackbar } from "../components/ErrorSnackbar/ErrorSnackbar";
+import { TodolistsList } from "features/TodolistsList/TodolistsList";
+import { ErrorSnackbar } from "components/ErrorSnackbar/ErrorSnackbar";
 import { useDispatch, useSelector } from "react-redux";
 import { AppRootStateType } from "./store";
 import { initializeAppTC, RequestStatusType } from "./app-reducer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login } from "../features/Login/Login";
-import { logoutTC } from "../features/Login/auth-reducer";
+import { Login } from "features/Login/Login";
+import { logoutTC } from "features/Login/auth-reducer";
 import {
   AppBar,
   Button,
@@ -19,21 +19,20 @@ import {
   Typography,
 } from "@mui/material";
 import { Menu } from "@mui/icons-material";
+// import { isLoadingSelector } from "app/app.selector";
+import { RootState } from "@reduxjs/toolkit/dist/query/core/apiState";
+import { isLoggedInSelector } from "app/app.selector";
 
 type PropsType = {
   demo?: boolean;
 };
 
 function App({ demo = false }: PropsType) {
-  const status = useSelector<AppRootStateType, RequestStatusType>(
-    (state) => state.app.status,
-  );
-  const isInitialized = useSelector<AppRootStateType, boolean>(
-    (state) => state.app.isInitialized,
-  );
-  const isLoggedIn = useSelector<AppRootStateType, boolean>(
-    (state) => state.auth.isLoggedIn,
-  );
+  const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status);
+  const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized);
+
+  const isLoggedIn = useSelector(isLoggedInSelector);
+
   const dispatch = useDispatch<any>();
 
   useEffect(() => {
